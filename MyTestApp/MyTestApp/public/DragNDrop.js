acab5c1f398bc52c1016b10de871528a9d1690dd
bananafish2828 <bananafish2828@gmail.com>
2017-08-20 15:24:57 -0700

    drag and drop array of items

--- MyTestApp/MyTestApp/public/DragNDrop.js
+++ MyTestApp/MyTestApp/public/DragNDrop.js
@@ -1,7 +1,16 @@
 import React, { Component } from 'react';
 import Dragula from 'react-dragula';
 
+import ListEntry from './ListEntry.jsx';
+
 export default class DragNDrop extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      testEntry: ['Swap me around', 'Swap her around', 'Swap him around']
+    }
+  }
 
   dragulaDecorator(componentBackingInstance) {
     if (componentBackingInstance) {
@@ -11,15 +20,17 @@ export default class DragNDrop extends Component {
   }
 
   render() {
+    console.log('length of entries', this.state.testEntry.length)
+    this.state.testEntry.map((entry, i) => {
+      console.log('entry #', i, ':', entry);
+    })
     return (
       <div className='container' ref={this.dragulaDecorator}>
-      <div>Swap me around</div>
-      <div>Swap her around</div>
-      <div>Swap him around</div>
-      <div>Swap them around</div>
-      <div>Swap us around</div>
-      <div>Swap things around</div>
-      <div>Swap everything around</div>
+        { this.state.testEntry.map((entry, i) => {
+          return <ListEntry entry={entry} key={i} /> 
+        })}
+        <div>test stuff goes here</div>
+        <div>more test stuff here</div>
       </div>
     )
   }
---
+++ MyTestApp/MyTestApp/public/ListEntry.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+const ListEntry = ({ entry }) => {
+
+  return (
+    <div>{entry}</div>
+  )
+}
+
+export default ListEntry;
